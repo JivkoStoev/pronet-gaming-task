ff9fa07d0d3ad13a29277b5eb4188bc479180688
DESKTOP-4LEEB5E\ACER <jivko_1990@mail.bg>
2024-11-11 11:24:09 +0200

    fix karma configs

---
+++ karma.conf.js
@@ -0,0 +1,29 @@
+// src/karma.conf.js
+module.exports = function (config) {
+  config.set({
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+    ],
+    client: {
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, '../coverage'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true,
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: true, // Ensures Karma exits after tests
+    failOnEmptyTestSuite: false, // Prevents errors if no tests are found
+  });
+};
